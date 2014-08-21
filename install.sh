b3f042bf308005598a662355c80dab9f03c30c9c
Hong Minhee <minhee@dahlia.kr>
2014-08-22 03:46:06 +0900

    Overwrite vimfiles if already exist

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git submodule update --init
-ln -s `pwd`/`dirname $0`/vimrc $HOME/.vimrc
-ln -s `pwd`/`dirname $0`/vim $HOME/.vim
+ln -sfi `pwd`/`dirname $0`/vimrc $HOME/.vimrc
+ln -sfi `pwd`/`dirname $0`/vim $HOME/.vim
 vim +PluginInstall +qall
