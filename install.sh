6983ea47d9141f69a1523820f792f6b8020ca4f4
Hong Minhee <hongminhee@member.fsf.org>
2016-05-13 03:22:46 +0900

    Create XDG config dir if not exist

--- install.sh
+++ install.sh
@@ -13,15 +13,17 @@ mkdir -p nvim/autoload
 curl -Lo nvim/autoload/plug.vim "$VIM_PLUG_URL"
 
 # Link neovim configuration to ~/.config/nvim
+xdg_config_dir="$HOME/.config"
 nvimrc_name="init.nvim"
 src_nvim_dir="$(pwd)/$(dirname "$0")/nvim"
 src_nvimrc="$src_nvim_dir/$nvimrc_name"
-dst_nvim_dir="$HOME/.config/nvim"
+dst_nvim_dir="$xdg_config_dir/nvim"
 dst_nvimrc="$dst_nvim_dir/$nvimrc_name"
 echo "Neovim configuration directory: $dst_nvim_dir"
 if [[ ! -f "$dst_nvimrc" || "$(cat "$src_nvimrc")" != "$(cat "$dst_nvimrc")" ]]
 then
   rm -f "$dst_nvim_dir"
+  mkdir "$xdg_config_dir"
   ln -sfi "$src_nvim_dir" "$dst_nvim_dir"
 fi
 
